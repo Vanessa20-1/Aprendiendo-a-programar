fc60864f16826b297d2c3bc5c39d16f76932781e
Vanessa20-1 <becerrajulieth69@gmail.com>
2026-08-27 12:14:17 -0500

    Aprendi a programar

--- mi_programa.py
+++ mi_programa.py
@@ -1,6 +1,6 @@
 print("Hello world")
-mi_nombre = "Vanessa"
-print("Hola, " + mi_nombre)
+mi_nombre = input("escriba su nombre: ")
+print("Hola \t" + mi_nombre)
 numero_1 = 5 
 numero_2 = 10
 print(numero_1 + numero_2)
