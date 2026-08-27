6208f0b8d2d00b9e0631f251450a56ebc20792f3
Vanessa20-1 <becerrajulieth69@gmail.com>
2026-08-27 12:37:29 -0500

    Mi primera pagina

--- mi_programa.py
+++ mi_programa.py
@@ -1,6 +1,7 @@
 print("Hello world")
 mi_nombre = input("escriba su nombre: ")
-print("Hola \t" + mi_nombre)
+print("Hola \t"+ mi_nombre.upper())
+
 numero_1 = 5 
 numero_2 = 10
 print(numero_1 + numero_2)
